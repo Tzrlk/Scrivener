3aab9969ca74c33f710050663c7aebd6da80a410
Peter Cummuskey <peterc@aetheric.co.nz>
2012-10-28 22:48:51 +1300

    Updating directory with work from ExoIDE.

---
+++ core/src/main/java/nz/co/aetheric/scrivener/user/UserBean.java
@@ -0,0 +1,35 @@
+package nz.co.aetheric.scrivener.user;
+
+public class UserBean {
+
+    Long id;
+    private String username;
+    private String password;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public UserBean setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public UserBean setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public UserBean setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+}
